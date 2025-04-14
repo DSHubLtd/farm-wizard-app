cedd674688c0c359449e55d552536e0859c728e6
avvas200 <avvassani@gmail.com>
2025-04-14 08:46:42 +0100

    App initial setup

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="bg-red-50 flex flex-1 justify-center items-center">
+      <Text className="text-2xl">WelCome!!!</Text>
+    </View>
+  );
+}
