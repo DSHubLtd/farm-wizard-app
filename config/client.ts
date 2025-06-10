cd1fc08e3d1ab1359bab4b9da1623584d1f8dbe4
avvas200 <avvassani@gmail.com>
2025-06-10 15:35:13 +0100

    All done

--- config/client.ts
+++ config/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// export const API_BASE = "https://farm-wizard-api-n68r.onrender.com";
-export const API_BASE = "http://192.168.1.73:5000";
+export const API_BASE = "https://farm-wizard-api-n68r.onrender.com";
+// export const API_BASE = "http://192.168.1.73:5000";
 
 // export default axios.create({ baseURL: "http://192.168.1.73:5000/api/v1" });
 export default axios.create({
